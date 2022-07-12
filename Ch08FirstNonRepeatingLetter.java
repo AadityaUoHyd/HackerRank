39325646f773026eb5211159ce02584299b42d92
Aaditya Bachchu Chatterjee <57300089+AadityaUoHyd@users.noreply.github.com>
2022-07-12 16:35:58 +0530

    Update Ch08FirstNonRepeatingLetter.java

--- Ch08FirstNonRepeatingLetter.java
+++ Ch08FirstNonRepeatingLetter.java
@@ -1,60 +1,38 @@
 package com.aadi.codingChallenges;
-// Find all anagrams in a String. Given a String s, and a non-empty String p, find all the start indices of p's anagrams in s. 
-// Strings consists of lower case English letters only and the length of both Strings s and p will not be larger than 20,100.
+// Given a String find the first non-repeatable character in the String. 
+// Example: If given input string is : "kkkkkooooovvvvuuupppqaaaajjjj"; Answer should be : q
 
-//Example Input:-         s: "cbaebabacd"            p:"abc"
-//Output : [0,6]
+import java.util.LinkedHashMap;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.io.*;
+import static java.util.function.Function.identity;
 
-//Explaination : The substring with start index 0 is 'cba' is anagram of p. The substring with start index 6 is 'bac' is anagram of p too.
-
-//Example Input:-         s: "abab"                  p:"ab"
-//Output : [0,1,2]
-
-
-import java.util.*;
-
-public class Ch17FindAllAnagramsInString{
-
-    public List<Integer> findAllAnagrams(String s, String p){
+public class Ch08FirstNonRepeatingLetter {
+    
+    
+    static String readInput;
+    
+    public static void main(String[] args) throws IOException {
         
-        List<Integer> result = new ArrayList<>();
+        System.out.println("Kindly Enter your string.");
+        InputStreamReader inputString=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(inputString);
+        readInput= br.readLine();
         
-        if(s.length() == 0 || s == null){
-            return result;
-        }
-        
-        int[] char_counts = new int[26];
-        for(char c : p.toCharArray()){
-            char_counts[c-'a']++;
-        }
-        
-        int left = 0;
-        int right = 0;
-        int count = p.length();
-        
-        while(right < s.length()){
-            
-            if(char_counts[s.charAt(right++)-'a']-- >= 1){
-                count--;
-            }
-            
-            if(count == 0) {
-                result.add(left);
-            }
-            
-            if(right-left==p.length() && char_counts[s.charAt(left++)-'a']++ >= 0){
-                count++;
-            }
-            
-        }
-        
-        return result;
-        
+        findFirstNonRepeatingLetter(System.out::println);
     }
-
-     public static void main(String []args){
-        Ch17FindAllAnagramsInString f = new Ch17FindAllAnagramsInString();
-        System.out.println(f.findAllAnagrams("cbaebabacd", "abc"));
-        System.out.println(f.findAllAnagrams("abab", "ab"));
-     }
-}
+    
+    private static void findFirstNonRepeatingLetter(Consumer<Character> callback) {
+        readInput.chars()
+                .mapToObj(i -> Character.valueOf((char) i))
+                .collect(Collectors.groupingBy(identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1L)
+                .map(entry -> entry.getKey())
+                .findFirst().map(c -> {
+            callback.accept(c);
+            return c;
+        });
+    }
+} 
