96d8ed0f40a1e4196987a005035538ebff5205b4
Aaditya Bachchu Chatterjee <57300089+AadityaUoHyd@users.noreply.github.com>
2022-08-27 23:54:37 +0530

    Update Ch17FindAllAnagramsInString.java

--- Ch17FindAllAnagramsInString.java
+++ Ch17FindAllAnagramsInString.java
@@ -1,13 +1,17 @@
-// Find all anagrams in a String. Given a String s, and a non-empty String p, find all the start indices of p's anagrams in s. 
-// Strings consists of lower case English letters only and the length of both Strings s and p will not be larger than 20,100.
+/*
 
-//Example Input:-         s: "cbaebabacd"            p:"abc"
-//Output : [0,6]
+  Find all anagrams in a String. Given a String s, and a non-empty String p, find all the start indices of p's anagrams in s. 
+  Strings consists of lower case English letters only and the length of both Strings s and p will not be larger than 20,100.
 
-//Explaination : The substring with start index 0 is 'cba' is anagram of p. The substring with start index 6 is 'bac' is anagram of p too.
+  Example Input:-         s: "cbaebabacd"            p:"abc"
+  Output : [0,6]
 
-//Example Input:-         s: "abab"                  p:"ab"
-//Output : [0,1,2]
+  Explaination : The substring with start index 0 is 'cba' is anagram of p. The substring with start index 6 is 'bac' is anagram of p too.
+
+  Example Input:-         s: "abab"                  p:"ab"
+  Output : [0,1,2]
+
+*/
 
 
 import java.util.*;
